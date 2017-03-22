857872243921d6c574b7138699da9c1a0f83c860
cuulkids <salifu@cuulkids.com>
2017-03-22 07:20:42 +0000

    Home layout getting there

--- website/src/components/Home.vue
+++ website/src/components/Home.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
     This is home
-    <playground></playground>
     <whistles></whistles>
+    <playground></playground>
   </div>
 </template>
+
 <script>
-	
+
+export default {
+    data() {
+      return {
+      	title: "This is home"
+      };
+  }
+}
+
 </script>
+<style type="scss">
+</style>
